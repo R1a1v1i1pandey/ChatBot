d8c27497f2214a42e8dc20fc20b4078832c6d6d4
Ravi Pandey <193773256+R1a1v1i1pandey@users.noreply.github.com>
2025-12-30 13:07:49 +0530

    add it Index.HTML

---
+++ Chat Bot/response.js
@@ -0,0 +1,12 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    hii: "How can I help you",
+    goodmorning: "Nice Day",
+    goodnight: "Sweet Dream",
+    ok: "By! Have a good day..",
+    tell: "this project is simple based on AI, which describe the short knowledge about AI",
+
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+};
